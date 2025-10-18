e5cc1bf07c5878fd3319cd1cc02963f6843098f0
achew <achew@cs.utexas.edu>
2025-10-18 16:57:35 -0500

    Made API key hidden

--- ai.js
+++ ai.js
@@ -1,27 +1,8 @@
-// import { GoogleGenAI } from "@google/genai";
-
-// const ai = new GoogleGenAI({});
-
-// async function main() {
-//   const response = await ai.models.generateContent({
-//     model: "gemini-2.5-flash",
-//     contents: "HI can you say hi so i know you work :D",
-//   });
-//   console.log(response.text);
-// }
-
-// await main();
-
-// TODO Not expose the api key
-
-// require('dotenv').config();
-
+import 'dotenv/config';
 import { GoogleGenAI } from "@google/genai";
 
-// const apiKey = process.env.GENAI_API_KEY;
-// console.log(`Your API Key: ${apiKey}`);
-
-const ai = new GoogleGenAI({ apiKey: "AIzaSyDCjT1wtl5zvzCZjGjGXWmd52N5eqDCq4E" });
+const apiKey = process.env.GENAI_API_KEY;
+const ai = new GoogleGenAI({ apiKey: apiKey });
 
 async function main() {
   const response = await ai.models.generateContent({
