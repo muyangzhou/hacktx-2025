1ce61c676a84772c46dfeb49a7a5aa7501e907dd
achew <achew@cs.utexas.edu>
2025-10-18 17:35:37 -0500

    Removed debug code

--- ai.js
+++ ai.js
@@ -6,8 +6,7 @@ import { GoogleGenAI } from "@google/genai";
 const apiKey = process.env.GENAI_API_KEY;
 const ai = new GoogleGenAI({ apiKey: apiKey });
 
-const inctructions = "Analyze the following list of transactions. \
-                      Respond only with a text analysis, not JSON."
+const inctructions = "Analyze the following list of transactions. Respond only with a text analysis, not JSON."
 
 const filePath = './test.json';
 
@@ -16,7 +15,8 @@ async function main()
   const jsonString = fs.readFileSync(filePath, 'utf8');
   const prompt = `${inctructions}\n\n--- TRANSACTION DATA ---\n${jsonString}`;
 
-  console.log(prompt);
+  // DEBUG print
+  // console.log(prompt);
 
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
