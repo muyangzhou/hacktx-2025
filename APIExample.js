aeee29cc28a97c4550b4c366f620c91b4944baf9
achew <achew@cs.utexas.edu>
2025-10-18 21:40:52 -0500

    Modified APIExample

--- APIExample.js
+++ APIExample.js
@@ -1,9 +1,16 @@
 import { promptAI } from './ai.js';
 import { getFormattedPurchaseHistory } from './nessie.js';
 
-const transactionData = await getFormattedPurchaseHistory("68f4283a9683f20dd51a012c");
+const transactionData = await getFormattedPurchaseHistory("68f426849683f20dd519ff49");
 const formattedTransactionData = JSON.stringify(transactionData, null, 2);
-const instructions = "Analyze the following list of transactions. Respond only with a text analysis, not JSON.";
-const prompt = `${instructions}\n\n--- TRANSACTION DATA ---\n${formattedTransactionData}`;
 
+console.log(formattedTransactionData);
+
+const instructions = "Analyze the following list of transactions in terms of \
+how healthy or unhealthy this list of transastions is. Give the overall list \
+of transactions a score value from 0 - 100, where higher scores represent healthy \
+transaction histories and lower scores represent unhealthy transaction histories. \
+Respond only with a text analysis, not JSON.";
+
+const prompt = `${instructions}\n\n--- TRANSACTION DATA ---\n${formattedTransactionData}`;
 console.log(await promptAI(prompt));
